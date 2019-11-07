f9ae7f144b42f1826e3f319f74ce678a31ebf31e
leslieba <56040833+leslieba@users.noreply.github.com>
2019-11-07 12:20:43 -0500

    Update game.js

--- static/game.js
+++ static/game.js
@@ -3,60 +3,25 @@ socket.on('message', function(data) {
   console.log(data);
 });
 
-var movement = {
-  up: false,
-  down: false,
-  left: false,
-  right: false
-}
-document.addEventListener('keydown', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      movement.left = true;
-      break;
-    case 87: // W
-      movement.up = true;
-      break;
-    case 68: // D
-      movement.right = true;
-      break;
-    case 83: // S
-      movement.down = true;
-      break;
-  }
-});
-document.addEventListener('keyup', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      movement.left = false;
-      break;
-    case 87: // W
-      movement.up = false;
-      break;
-    case 68: // D
-      movement.right = false;
-      break;
-    case 83: // S
-      movement.down = false;
-      break;
-  }
-});
-
+let plx= 50;
+let ply = 50;
 function setup() {
   createCanvas(800, 600);
   socket.emit('new player');
 }
 
 // This is basically the "draw" function
-socket.on('state', function(players) {
+// socket.on('state', function(players) {
+function draw(){
   background(200);
+let c = dist(mouseX,mouseY, plx, ply);
+let d = constrain(c,0,100);
 
+let x = -((d/c) * (plx-mouseX))+plx
+let y = -((d/c) * (ply-mouseY))+ply
   // For every player object sent by the server...
-  for (var id in players) {
-    var player = players[id];
-    ellipse(player.x, player.y, 10); // Draw an ellipse
-  }
 
+ellipse(x,y,10)
+ellipse(plx,ply,50);
   // Send movement data to the server
-  socket.emit('movement', movement);
-});
+}
