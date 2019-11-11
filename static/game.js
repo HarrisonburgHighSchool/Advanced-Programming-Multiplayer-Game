6f16379c245a9876c352b17f30cfaf9db5536743
leslieba <56040833+leslieba@users.noreply.github.com>
2019-11-11 11:55:55 -0500

    Update game.js

--- static/game.js
+++ static/game.js
@@ -1,13 +1,19 @@
+
 var socket = io();
+
 socket.on('message', function(data) {
   console.log(data);
 });
 
 let plx= 50;
 let ply = 50;
+let bubble;
+
 function setup() {
   createCanvas(800, 600);
   socket.emit('new player');
+  bubble = new Bubble();
+  print(bubble.x,bubble.y);
 }
 
 // This is basically the "draw" function
@@ -23,5 +29,32 @@ let y = -((d/c) * (ply-mouseY))+ply
 
 ellipse(x,y,10)
 ellipse(plx,ply,50);
-  // Send movement data to the server
+
+  //Send movement data to the server
+bubble.move();
+bubble.show();
+
+
+}
+// function show() {
+//   stroke(255);
+//   strokeweight(4);
+//   noFill();
+//   ellipse(bubble.x, bubble.y, 24, 24);
+// }
+class Bubble{
+  constructor(){
+this.x = 200;
+this.y = 200;
+}
+move() {
+   bubble.x = bubble.x + random (-5,5);
+   bubble.y = bubble.y + random (-5,5);
+  }
+  show() {
+    stroke(255);
+    strokeweight(20);
+    noFill();
+    ellipse(bubble.x, bubble.y, 24, 24);
+  }
 }
