b0778ce52b42586f22e1c92cfcb91f75e61c2757
oxcorr21 <47217740+oxcorr21@users.noreply.github.com>
2019-11-13 12:55:06 -0500

    Update game.js

--- static/game.js
+++ static/game.js
@@ -3,60 +3,27 @@ socket.on('message', function(data) {
   console.log(data);
 });
 
-var movement = {
-  up: false,
-  down: false,
-  left: false,
-  right: false
-}
-document.addEventListener('keydown', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      movement.left = true;
-      break;
-    case 87: // W
-      movement.up = true;
-      break;
-    case 68: // D
-      movement.right = true;
-      break;
-    case 83: // S
-      movement.down = true;
-      break;
-  }
-});
-document.addEventListener('keyup', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      movement.left = false;
-      break;
-    case 87: // W
-      movement.up = false;
-      break;
-    case 68: // D
-      movement.right = false;
-      break;
-    case 83: // S
-      movement.down = false;
-      break;
-  }
-});
+
 
 function setup() {
   createCanvas(800, 600);
-  socket.emit('new player');
+
 }
 
 // This is basically the "draw" function
 socket.on('state', function(players) {
   background(200);
-
-  // For every player object sent by the server...
-  for (var id in players) {
-    var player = players[id];
-    ellipse(player.x, player.y, 10); // Draw an ellipse
+  fill(255);
+  ellipse(400, 300, 50);
+  noFill();
+  ellipse(400, 300, 200)
+  if(dist(mouseX, mouseY, 400, 300) < 100) {
+    ellipse(mouseX, mouseY, 50, 50);
+  } else {
+    let x = (dist(mouseX,mouseY,400,300)/100)*(mouseX-400);
+    let y = (dist(mouseX,mouseY,400,300)/100)*(mouseY-300);
+    ellipse(x, y, 50, 50);
   }
+  line(mouseX, mouseY,400, 300);
 
-  // Send movement data to the server
-  socket.emit('movement', movement);
 });
