5e6495c4cb8d3342ca0c653ad658545fc0686203
Alex <axsort99@harrisonburg.k12.va.us>
2020-01-17 12:08:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -392,67 +392,67 @@ setInterval(function() {
 // }, 1000 / 60);
 
 
-class Bullet {
- constructor(player, mx, my) {
-    mx = mx + player.x+7 - 450 //mouse x + player x - half of screen width
-    my = my + player.y+9.5 - 400 //gives us mouse relative to player
-    this.pl_id = player.id;
-    this.x = player.x+7-5;
-    this.y = player.y+9.5-5;
-    this.tempx = mx - player.x;
-    this.tempy = my - player.y;
-    this.orientation = Math.atan(this.tempy/this.tempx);
-
-    if (player.x > mx) {
-      this.dy = -Math.sin(this.orientation)*15;
-      this.dx = -Math.cos(this.orientation)*15;
-    } else {
-      this.dy = Math.sin(this.orientation)*15;
-      this.dx = Math.cos(this.orientation)*15;
-    }
-    // bullet needs to start not inside the players
-    this.x = this.x + (this.dx*2);
-    this.y = this.y + (this.dy*2);
-  }
-}
-
-class Player {
-  constructor(id) {
-    this.x = 450;
-    this.y = 400;
-    this.right = false;
-    this.left = false;
-    this.up = false;
-    this.down = false;
-    this.id = id;
-    this.teamid = nextteamselect
-    if (this.teamid == 0) {
-      nextteamselect = 1
-    }
-    if (this.teamid == 1) {
-      nextteamselect = 0
-    }
-    if (this.teamid == 0) {
-      this.x = 100;
-    } else {
-      this.x = 500;
-    }
-    if (this.teamid == 0) {
-      this.y = 100;
-    } else {
-      this.y = 500;
-    }
-    this.hp = 10
-    this.r = 10
-  }
-}
-
-class Waypoint {
-  constructor(x,y,t) {
-    this.x = x
-    this.y = y
-    this.r = 25
-    this.team = t
-    this.points = 50
-  }
-}
+// class Bullet {
+//  constructor(player, mx, my) {
+//     mx = mx + player.x+7 - 450 //mouse x + player x - half of screen width
+//     my = my + player.y+9.5 - 400 //gives us mouse relative to player
+//     this.pl_id = player.id;
+//     this.x = player.x+7-5;
+//     this.y = player.y+9.5-5;
+//     this.tempx = mx - player.x;
+//     this.tempy = my - player.y;
+//     this.orientation = Math.atan(this.tempy/this.tempx);
+//
+//     if (player.x > mx) {
+//       this.dy = -Math.sin(this.orientation)*15;
+//       this.dx = -Math.cos(this.orientation)*15;
+//     } else {
+//       this.dy = Math.sin(this.orientation)*15;
+//       this.dx = Math.cos(this.orientation)*15;
+//     }
+//     // bullet needs to start not inside the players
+//     this.x = this.x + (this.dx*2);
+//     this.y = this.y + (this.dy*2);
+//   }
+// }
+//
+// class Player {
+//   constructor(id) {
+//     this.x = 450;
+//     this.y = 400;
+//     this.right = false;
+//     this.left = false;
+//     this.up = false;
+//     this.down = false;
+//     this.id = id;
+//     this.teamid = nextteamselect
+//     if (this.teamid == 0) {
+//       nextteamselect = 1
+//     }
+//     if (this.teamid == 1) {
+//       nextteamselect = 0
+//     }
+//     if (this.teamid == 0) {
+//       this.x = 100;
+//     } else {
+//       this.x = 500;
+//     }
+//     if (this.teamid == 0) {
+//       this.y = 100;
+//     } else {
+//       this.y = 500;
+//     }
+//     this.hp = 10
+//     this.r = 10
+//   }
+// }
+//
+// class Waypoint {
+//   constructor(x,y,t) {
+//     this.x = x
+//     this.y = y
+//     this.r = 25
+//     this.team = t
+//     this.points = 50
+//   }
+// }
